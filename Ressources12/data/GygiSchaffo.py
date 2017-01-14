b211d25eb3e8285c4a738d7c6a77d65f12922826
Schaffora <schaffora@gmail.com>
2017-01-14 13:40:49 +0100

    IA Terminaison

--- Ressources12/data/GygiSchaffo.py
+++ Ressources12/data/GygiSchaffo.py
@@ -9,7 +9,7 @@ import time
 
 SCREEN_X = 500
 SCREEN_Y = 500
-CITY_COLOR = [10, 10, 200]
+CITY_COLOR = [248, 255, 11]
 CITY_RADIUS = 7
 FONT_COLOR = [255, 255, 255]
 
@@ -39,9 +39,8 @@ class City:
         return str(self).__hash__()
 
 class Individu:
-    def __init__(self, cities,startCity):
+    def __init__(self, cities):
         self.travelPath=cities
-        self.startCity=startCity
         self.distance= 0
 
     def totalDistance(self):
@@ -54,46 +53,7 @@ class Individu:
             self.distance += int(math.sqrt((int(city1.x) - int(city.x)) ** 2 + (int(city1.y) - int(city.y)) ** 2))
             
     def __str__(self):
-        return "The total distance calculated using travel path is: " + '%s' % (self.distance) + " " + '%s' % (self.travelPath)
-
-def ga_solve(file=None, gui=True, maxTime=0):
-
-    if gui is True:
-        if file is not None:
-            readFile(file)
-        initPoints(file)
-
-    citiesToVisit=deepcopy(cities)
-    startCity= citiesToVisit.pop(0)
-    for i in range(0, POPULATION_SIZE):
-        newIndividu = Individu(random.sample(citiesToVisit,len(citiesToVisit)),startCity)
-        newIndividu.travelPath.insert(0,startCity)
-        newIndividu.totalDistance();
-        individuExist(newIndividu)
-
-    individues.sort(key=lambda x: x.distance, reverse=False)
-    numberOfIndividues=len(individues)
-
-    startTime = time.time()
-
-    while time.time()<startTime+maxTime:
-        screen.fill(0)
-        selection()
-        mutation()
-        individues.sort(key=lambda x: x.distance, reverse=False)
-        while(len(individues)>numberOfIndividues):
-            individues.pop()
-        drawLine(individues[0].travelPath)
-
-def selection():
-    sampleSize=int(len(individues)*0.2)
-    selectedIndividues=[]
-    for i in range(0,sampleSize):
-        selectedIndividues.append(individues[i])
-        selectedIndividues.append(individues[random.randint(sampleSize, len(individues)-1)])
-
-    for i in range(0,sampleSize):
-        croisement(selectedIndividues[i],selectedIndividues[len(selectedIndividues)-i-1],len(selectedIndividues[0].travelPath))
+        return "Distance of found path: " + '%s' % (self.distance)
 
 def readFile(file):
     with open(file,"r") as f:
@@ -129,11 +89,63 @@ def initPoints(file):
         event = pygame.event.wait()
         if event.type == KEYDOWN: break
 
+
+
+def ga_solve(file=None, gui=True, maxTime=0):
+    if gui is True:
+        if file is not None:
+            readFile(file)
+        initPoints(file)
+
+    citiesToVisit=deepcopy(cities)
+    startCity= citiesToVisit.pop(0)
+    for i in range(0, POPULATION_SIZE):
+        newIndividu = Individu(random.sample(citiesToVisit,len(citiesToVisit)))
+        newIndividu.travelPath.insert(0,startCity)
+        newIndividu.totalDistance();
+        individuExist(newIndividu)
+
+    individues.sort(key=lambda x: x.distance, reverse=False)
+    numberOfIndividues=len(individues)
+
+    startTime = time.time()
+
+    while time.time()<startTime+maxTime:
+        screen.fill(0)
+        selection()
+        mutation()
+        individues.sort(key=lambda x: x.distance, reverse=False)
+        while(len(individues)>numberOfIndividues):
+            individues.pop()
+        drawLine(individues[0].travelPath)
+
+    while True:
+        screen.fill(0)
+        text = font.render(str(individues[0]), True, FONT_COLOR)
+        textRect = text.get_rect()
+        screen.blit(text, textRect)
+        pygame.display.flip()
+        event = pygame.event.wait()
+        if event.type == KEYDOWN: break
+
+    return individues[0]
+
+
+def selection():
+    sampleSize=int(len(individues)*0.4)
+    selectedIndividues=[]
+    for i in range(0,sampleSize):
+        selectedIndividues.append(individues[i])
+        selectedIndividues.append(individues[random.randint(sampleSize, len(individues)-1)])
+
+    for i in range(0,sampleSize):
+        croisement(selectedIndividues[i],selectedIndividues[len(selectedIndividues)-i-1],len(selectedIndividues[0].travelPath))
+        croisement(selectedIndividues[len(selectedIndividues) - i - 1], selectedIndividues[i],len(selectedIndividues[0].travelPath))
+
 def croisement(indiv1, indiv2, numberOfCity):
-    swapSize= int(numberOfCity*0.3)
+    swapSize= int(numberOfCity*0.4)
     newCityTravelpath=[]
     newCityStartCity=indiv1.travelPath[0]
-
     for i in range(0,swapSize):
         newCityTravelpath.append(indiv1.travelPath[i])
 
@@ -141,23 +153,36 @@ def croisement(indiv1, indiv2, numberOfCity):
         if cityIndiv2 not in newCityTravelpath:
             newCityTravelpath.append(cityIndiv2)
     newCityTravelpath.pop(0)
-    newIndividu = Individu(newCityTravelpath, newCityStartCity)
+    newIndividu = Individu(newCityTravelpath)
     newIndividu.travelPath.insert(0, newCityStartCity)
     newIndividu.totalDistance();
     individuExist(newIndividu)
 
 def mutation():
     for i in range(0,4):
-        if (random.random() < 0.1):
+        if (random.random() < 0.8):
             index = random.randint(0, len(individues) - 1)
-            indiv = individues[index]
+            indiv =deepcopy(individues[index])
             listOrder = []
             listOrder.extend(indiv.travelPath)
-            first = listOrder.pop(2)
-            last = listOrder.pop(3)
-            listOrder.insert(2, last)
-            listOrder.insert(3, first)
+            r1=random.randint(1,len(indiv.travelPath)-2)
+            r2=random.randint(1,len(indiv.travelPath)-2)
+            first = listOrder.pop(r1)
+            last = listOrder.pop(r2)
+            listOrder.insert(r1, last)
+            listOrder.insert(r2, first)
             indiv.travelPath = listOrder
+            indiv.totalDistance();
+            if( indiv.distance < individues[index].distance):
+                individues.append(indiv)
+
+def individuExist(indiv):
+    pathExist = False
+    for individu in individues:
+        if (individu.travelPath == indiv.travelPath):
+            pathExist = True
+    if (pathExist is not True):
+        individues.append(indiv)
 
 def draw(cityList):
     screen.fill(0)
@@ -184,17 +209,9 @@ def drawLine(cityList):
         pygame.draw.lines(screen, CITY_COLOR, True, cityLine)
         pygame.display.flip()
 
-def individuExist(indiv):
-    pathExist = False
-    for individu in individues:
-        if (individu.travelPath == indiv.travelPath):
-            pathExist = True
-    if (pathExist is not True):
-        individues.append(indiv)
-
 
 if __name__ == '__main__':
-    ga_solve("pb020.txt", True, 60)
+    ga_solve("pb050.txt", True, 10)
 
 
 
